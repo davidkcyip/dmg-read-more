047dd989001fb1d865affb0113ee5e7c4568f2dd
davidkcyip <davidkcyip@gmail.com>
2024-06-23 16:20:51 +0100

    Remove build folder

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9232b3035c5d5bbb1f5b');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '3ec2d742128dfbd90e5e');
